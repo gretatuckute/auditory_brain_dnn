0c56f344ea0cfcbade14b3e314ab7849b67b4ae4
Greta Tuckute <gretatu@mit.edu>
2023-09-15 09:52:46 -0400

    Update download_files.py to include separate folder for surface maps

--- setup_utils/download_files.py
+++ setup_utils/download_files.py
@@ -7,9 +7,10 @@ from os.path import join
 ROOT = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 
 # Use the flags below to indicate what you want to download. The flags get_data and get_model_actv are sufficient to run your own models.
-get_data = True
+get_data = True ~180MB
 get_model_actv = False # Obs: large file (~2.7GB)
 get_results = False # Obs: large file (~2.1GB)
+get_fsavg_surf = False # ~480MB
 
 print(f'Downloading files to {ROOT}')
 
@@ -39,7 +40,7 @@ def download_extract_remove(url, extract_location):
     print('Removing temp file %s'%temp_file_location)
     os.remove(temp_file_location)
 
-# Download the data folder (~480MB). This folder contains the two neural datasets (NH2015, B2021) and the component dataset (NH2015comp)
+# Download the data folder (~180MB). This folder contains the two neural datasets (NH2015, B2021) and the component dataset (NH2015comp)
 if get_data:
     url_data_folder = 'https://mcdermottlab.mit.edu//tuckute_feather_2023/data.tar'
     download_extract_remove(url_data_folder, ROOT)
@@ -53,3 +54,8 @@ if get_model_actv:
 if get_results:
     url_results_folder = 'https://mcdermottlab.mit.edu//tuckute_feather_2023/results.tar'
     download_extract_remove(url_results_folder, ROOT)
+
+# Download the fsavg_surf folder (~480MB). This folder contains the surface-projected layer position maps for each model.
+if get_fsavg_surf:
+    url_fsavg_surf_folder = 'https://mcdermottlab.mit.edu//tuckute_feather_2023/fsavg_surf.tar'
+    download_extract_remove(url_fsavg_surf_folder, ROOT)
